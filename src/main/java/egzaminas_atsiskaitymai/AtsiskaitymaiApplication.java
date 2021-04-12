51b0206fae506778c95306d03b2c9ce2c1618f3d
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2021-04-12 09:06:07 +0300

    pradinis sugeneruotas kodas

---
+++ src/main/java/egzaminas_atsiskaitymai/AtsiskaitymaiApplication.java
@@ -0,0 +1,13 @@
+package egzaminas_atsiskaitymai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsiskaitymaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsiskaitymaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/egzaminas_atsiskaitymai/AtsiskaitymaiApplicationTests.java
@@ -0,0 +1,13 @@
+package egzaminas_atsiskaitymai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsiskaitymaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
